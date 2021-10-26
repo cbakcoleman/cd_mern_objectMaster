91e3f8149a3d14d20bc96c67ef9720d2aad3243c
cbakcoleman <88118758+cbakcoleman@users.noreply.github.com>
2021-10-26 16:12:46 -0700

    complete challenges

--- objectMaster.js
+++ objectMaster.js
@@ -26,18 +26,43 @@ const pokémon = Object.freeze([
 
     
     // an array of pokemon objects where the id is evenly divisible by 3
-    const divByThree 
+    const divByThreeIds = pokémon.filter( p => p.id % 3 === 0);
+    console.log("Pokemon with ID evenly divisible by three: ");
+    console.log(divByThreeIds);
 
     // an array of pokémon objects that are "fire" type
+    const fireTypes = pokémon.filter( p => p.types.includes("fire"));
+    console.log("Pokemon that are 'fire' type:");
+    console.log(fireTypes);
 
     // an array of pokémon objects that have more than one type
+    const multiTypes = pokémon.filter( p => p.types[1] != null);
+    console.log("Pokemon that have more than one type: ");
+    console.log(multiTypes);
 
     // an array with just the names of the pokémon
+    const pokemonNames = pokémon.map( p => p.name);
+    console.log("The names of the pokemon are:");
+    console.log(pokemonNames);
+
 
     // an array with just the names of pokémon with an id greater than 99
+    const above99ids = pokémon.filter( p => p.id > 99);
+    console.log("Pokemon with ids greater than 99:");
+    console.log(above99ids);
 
     // an array with just the names of the pokémon whose only type is poison
+    const poisonTypes = pokémon.filter( p => p.types[0] == "poison" && p.types[1] == null);
+    console.log("Pokemon whose only type is poison:");
+    console.log(poisonTypes);
 
     // an array containing just the first type of all the pokémon whose second type is "flying"
+    const flyingSecondTypes = pokémon.filter( p => p.types[1] == "flying");
+    console.log("Pokemon whose second type is flying:");
+    console.log(flyingSecondTypes);
 
     // a count of the number of pokémon that are "normal" type
+    let count = 0;
+    const numOfNorms = pokémon.filter( p => p.types.includes("normal")).map( count => count++ );
+    console.log("Number of pokemon that are 'normal' type: ");
+    console.log(numOfNorms);
